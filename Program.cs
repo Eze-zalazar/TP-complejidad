fc88a9facf1695285e9684807ca9ca90ace931e6
EzequielZalazar <urielezequielzalazar@gmail.com>
2024-10-09 21:52:52 -0300

    se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: eze
+ * Fecha: 9/10/2024
+ * Hora: 21:09
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace tp_final_complejidad
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
